544cb9de5e73491d1c69cd378f00e89dd618f04f
jonatas-pereira <jonataspereirarocha@gmail.com>
2025-11-24 00:41:03 -0300

    refactor: corrigindo imports

--- src/modules/saque/saque.controller.ts
+++ src/modules/saque/saque.controller.ts
@@ -1,4 +1,4 @@
-import { Body, ConflictException, Controller, Post } from "@nestjs/common";
+import { Body, Controller, HttpCode, Post } from "@nestjs/common";
 import { SaqueDto } from "./dto/saque.dto";
 import { SaqueService } from "./saque.service";
 
@@ -7,6 +7,7 @@ export class SaqueController {
   constructor(private readonly saqueService: SaqueService) { }
 
   @Post()
+  @HttpCode(200)
   public calcQtdCedulas(@Body() { valor }: SaqueDto) {
     return this.saqueService.calcQtdCedulas(valor)
   }
